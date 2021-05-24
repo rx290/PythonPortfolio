3e18d51041d60bb8a399198fb8cc084a655dcd47
rx290 <asadwaseem32@gmail.com>
2021-05-24 07:03:19 +0500

    hangman completed

--- 100 Days of Python/Day7-Hangman/hangman.py
+++ 100 Days of Python/Day7-Hangman/hangman.py
@@ -1,25 +1,130 @@
 from random import randrange as randomizer
+import  os, time, re
+
 
 word_list = ["pills","bills","bill","gills","gill","drills","drill","dark"]
 
 word = word_list[randomizer(0,len(word_list))]
-print(word)
+
+
+stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''','''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''',]
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+print(logo)
+time.sleep(2)
+os.system('clear')
+
 blanks = []
 
 for x in range(0,len(word)): 
     blanks.append('_') 
-
-print(blanks)
-
-
-
 tries = 0
-for i in range(0,6):
-    user_input = input("Please Enter your desired character: ")
-    if user_input in word:
-        index = word.find(user_input)
-        blanks[index] = user_input
-        print(blanks)
+counter = 0
+print(blanks)
+print(stages[tries])
+
+
+
+while(tries <= 6):
+    if(tries ==6):
+        print("You loose!")
+        break
     else:
-        tries +=1
-        print("You've exhausted {tries} out of 6 tries.", tries)
+        user_input = input("Please Enter your desired character: ")
+        def duplicates(lst, user_input):
+            return [i for i, x in enumerate(lst) if x == user_input]
+        if user_input in word and (word.count(user_input)<2):
+            counter +=1
+            index = word.find(user_input)
+            blanks[index] = user_input
+            print(blanks)
+            print(stages[tries])
+            if '_' not in blanks:
+                print("you've won!")
+                break
+            time.sleep(1)
+            os.system('clear')
+        elif user_input in word and (word.count(user_input)>=2):
+            indexer = duplicates(word,user_input)
+            counter += len(indexer)
+            for x in range(len(indexer)):
+                blanks[indexer[x]] = user_input
+                print(blanks)
+                print(stages[tries])
+                if '_' not in blanks:
+                    print("you've won!")
+                    break
+                time.sleep(1)
+                os.system('clear')
+               
+        else:
+
+            tries +=1
+            print("You've exhausted "+ str(tries) +" out of 6 tries.")
+            print(stages[tries])
+            time.sleep(1)
+            os.system('clear')
