c648547818171a84e0ef83ab4e907696c02281f2
rx290 <asadwaseem32@gmail.com>
2022-03-14 18:18:49 +0500

    Added all 15 questions for basics programming practise

--- Basics/Problem_10.py
+++ Basics/Problem_10.py
@@ -4,4 +4,12 @@ a)  g = big / 2 * 4 / big - big + abc /3;
     (abc =  2.5, big = 2 , assume g to be a float)
     
 b)  on = ink * act /2 + 3 /2 * act + 2 + toh;
-    (int = 4, act = 1, tig =3.2, assume on to be an int)"""
+    (int = 4, act = 1, tig =3.2, assume on to be an int)
+    
+c)  s = qui * add / 4 - 6 / 2 + 2 / 3 * 6 / god;
+    (qui = 4, add = 2, god =2, assume s to be an int)
+
+d)  s - 1 / 3 * a / 4 - 6 / 2 + 2 /3 *6 / g;
+    ( a = 4, g = 3, assume s to be an int)
+
+"""
