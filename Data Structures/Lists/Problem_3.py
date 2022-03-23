6d311dfb945e66a05533fb51947409d7a9b29e3d
rx290 <asadwaseem32@gmail.com>
2022-03-23 22:34:31 +0500

    Added 1st problem of list pset

--- Data Structures/Lists/Problem_3.py
+++ Data Structures/Lists/Problem_3.py
@@ -4,3 +4,14 @@ Original List:
 [25, 45, 36, 47, 69, 48, 68, 78, 14, 36]
 Sorted List:
 [14, 25, 36, 36, 45, 47, 48, 68, 69, 78] """ 
+
+num = int(input("Please enter the number of elements you want to add: "))
+
+_list = []
+
+for x in range(0,num):
+    a = int(input("Please enter your "+str(x+1)+ " element: "))
+    _list.append(a)
+    
+print("Original List: ", _list)
+print("Sorted List: ", sorted(_list))
