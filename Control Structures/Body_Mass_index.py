b215511235610e92844d3a5a5669cb402529067f
rx290 <asadwaseem32@gmail.com>
2022-03-27 13:16:01 +0500

    Added BMI

--- Control Structures/Body_Mass_index.py
+++ Control Structures/Body_Mass_index.py
@@ -7,3 +7,34 @@ Normal          18.5 to 24.9
 Overweight      25 tp 29.9
 Obese           30 or greater
 """
+
+def Kilo_converstion():
+    weight_in_kilos = float(input("Enter your weight is kilos: "))
+    weight_in_pounds = weight_in_kilos * 2.205
+    return weight_in_pounds
+    
+def feet_to_inches():
+    feet = float(input("Please enter your height in feet (eg. 5.9 feet): "))
+    inches = feet * 12
+    return inches
+    
+def feet_to_centimeter(feet):
+    centimeter = feet * 30.48
+    return centimeter
+
+def BMI():
+    x = input("Do you have your weight in pounds? (yes/no): ")
+    y = input("Do you have your height in feet? (yes/no): ")
+    weight = float(input("Please enter your weight in pounds: ")) if x=="yes" else Kilo_converstion()
+    height = float(input("Please enter your weight in inches: ")) if x=="yes" else feet_to_inches()
+    
+    bmi = ( weight * 703 ) / (height * height)
+    
+    if bmi < 18.5:
+        print("You're underweight! Eat something will ya?")
+    elif bmi > 18.5 and bmi < 24.9:
+        print("You're normal!")
+    elif bmi > 25 and bmi < 30:
+        print("You're overweight! Get a grip!")
+    else:
+        print("You're Obese, Shred some weight bruh!")
--- Data Structures/Stack/Problem_1.py
+++ Data Structures/Stack/Problem_1.py
@@ -1,1 +1,3 @@
-"""Apply the postfix algorithm to the expression 1 2 + 3 *.This example demonstrates one of the advantages of postfix—there is no need to use parentheses to control the order of operations. To get the same result in infix, we would have to write (1 + 2) * 3. """ 
+"""Apply the postfix algorithm to the expression 1 2 + 3 *.
+This example demonstrates one of the advantages of postfix—there is no need to use parentheses to control 
+the order of operations. To get the same result in infix, we would have to write (1 + 2) * 3. """ 
