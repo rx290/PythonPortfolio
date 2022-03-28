4caefcbfecf4cb25c113d9d5cb16ae232a6284bc
rx290 <asadwaseem32@gmail.com>
2022-03-29 00:50:10 +0500

    added functional programming pset

--- Functional Programming/Problem_1.py
+++ Functional Programming/Problem_1.py
@@ -1,3 +1,14 @@
 """
 Write a recursive function to obtain the running sum of first 25 natural numbers.
 """
+sum = 0
+
+def nn_sum(x):
+    #using global variaable
+    global sum 
+    sum = sum + x
+    print("Sum is equal to: ",sum) if x==0 else nn_sum(x-1)
+
+# Modified it to take input and return answer
+num = int(input("Please enter the natural number to find sum of: "))
+nn_sum(num)
