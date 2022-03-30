e5eebe424d79b922534e2f4a89be7b205d51c222
rx290 <asadwaseem32@gmail.com>
2022-03-31 00:35:49 +0500

    added some PSET for Control Structure

--- Control Structures/Problem_10.py
+++ Control Structures/Problem_10.py
@@ -9,3 +9,10 @@ a - z                  97 - 122
 0 - 9                  48 - 77
 special character      0 - 47, 58 - 64 , 91 - 96, 123 - 127
 """
+text = input("Please enter a string: ")
+ascii_text = ''
+for i in text:
+    print("Char: ",i ," Ascii Representation: ",str(ord(i)))
+    ascii_text += str(ord(i))
+    
+print(ascii_text)
