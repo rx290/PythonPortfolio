90fd4fee1c4e144abed7991aa730745e91dab5dc
rx290 <asadwaseem32@gmail.com>
2022-04-06 01:26:32 +0500

    added more solutions to basic programs

--- Basics/Problem_14.py
+++ Basics/Problem_14.py
@@ -4,3 +4,9 @@ The German deutschemark was $0.548, and the japanese yen was $0.00955.487
 Write a program that allows the user to enter an amount in dollars, and then display these value converted,
 in the above mentioned monetary units
 """
+
+pound, franc, deutschemark, yen = 1.487, 0.172, 0.548, 0.009555
+
+dollars = float(input("Please enter amount in dollars: "))
+
+print("Converted Rates:\nBritish Pound: {:.3f}\nFrench Franc: {:.3f}\nGerman Deutschemark: {:.3f}\nJapanese Yen: {:.5f}".format(pound*dollars, franc*dollars,deutschemark*dollars,yen*dollars))
--- Basics/Problem_15.py
+++ Basics/Problem_15.py
@@ -1,3 +1,9 @@
 """You can convert temperature from degree celsius to degree fahrenheit by multiplying
 by 9/5 and adding 32. write a program that takes users input in float representing Celsius and
 then convert it and display the corresponding degree in Fahrenheit"""
+
+celsius = float(input("Please enter temperature in Celsius: "))
+
+fahrenheit = (9/5 * celsius) + 32
+
+print("{} celsius is {} in fahrenheit.".format(celsius,fahrenheit))
