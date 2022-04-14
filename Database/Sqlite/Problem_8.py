d7a6e1b43df759465f4123ca4c83a897ff3ac0a5
rx290 <asadwaseem32@gmail.com>
2022-04-15 02:37:17 +0500

    added some pset solution

--- Database/Sqlite/Problem_8.py
+++ Database/Sqlite/Problem_8.py
@@ -1,1 +1,19 @@
 """ Write a Python program to count the number of rows of a given SQLite table.""" 
+import sqlite3 
+from sqlite3 import Error
+from sre_parse import State 
+
+connection = None
+database_name = input("Please enter database name: ")
+try:
+    connection = sqlite3.connect(database_name+'.db')
+    cursor = connection.cursor()
+    
+    statement_exec = cursor.execute('select count( * ) From products;')
+    print(statement_exec)
+except Error as e:
+      print(e)
+finally:
+    if connection:
+        connection.close()
+ 
