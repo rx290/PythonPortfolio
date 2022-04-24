009c540670c6c69e7799659b4022550da960a275
rx290 <asadwaseem32@gmail.com>
2022-04-25 00:56:23 +0500

    modified some control structures pset

--- Control Structures/Problem_2.py
+++ Control Structures/Problem_2.py
@@ -1,6 +1,11 @@
 """Write a program that prints the minimum of four input integers"""
 numbers = []
+min = 0
 for i in range(4):
     numbers.append(int(input("Please enter {} number: ".format(i+1))))
-    
-print("Minimum of these {} numbers is: {}".format(numbers, min(numbers)))
+    if i == 0:
+        min = numbers[i]
+    else:
+        pass
+# print("Minimum of these {} numbers is: {}".format(numbers, min(numbers)))
+
--- Control Structures/Problem_4.py
+++ Control Structures/Problem_4.py
@@ -6,6 +6,12 @@ mark_age = int(input("Hey Mark, Please enter your age: "))
 david_age = int(input("Hey David, Please enter your age: "))
 tom_age = int(input("Hey Tom, Please enter your age: "))
 
-ages = {mark_age:"Mark",david_age:"David",tom_age:"Tom"}
+if (mark_age<david_age and mark_age<tom_age):
+    print("Mark is the youngest!")
+elif (david_age<mark_age and david_age<tom_age):
+    print("David is the youngest!")
+else:
+    print("Tom is the youngest")
+# ages = {mark_age:"Mark",david_age:"David",tom_age:"Tom"}
 
-print("The youngest one is: {}".format(ages[min(ages)]))
+# print("The youngest one is: {}".format(ages[min(ages)]))
