9b1b963750593ae8722cd171f17af48a9703a0ca
rx290 <asadwaseem32@gmail.com>
2022-04-27 23:53:24 +0500

    some solution pset added

--- Control Structures/Problem_19.py
+++ Control Structures/Problem_19.py
@@ -1,3 +1,13 @@
 """
 Write a prgogram that takes a five digit number from the user and print its reverse using a while loop
 """
+digits= input("Please enter a five characer long digit: ")
+
+reverse_digits =""
+i = 5
+while i <= 1:
+    reverse_digits = reverse_digits + digits[i]
+    i = i - 1
+    
+print("digit: ",digits)
+print("reversed digits: ",reverse_digits) 
