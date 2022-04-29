9097b6748ac461fd8571b05e139221366690f002
rx290 <asadwaseem32@gmail.com>
2022-04-29 23:25:28 +0500

    added some solution to CS pset

--- Control Structures/Problem_22.py
+++ Control Structures/Problem_22.py
@@ -10,3 +10,14 @@ Example:
     MPG this trip: 22.076923
     Total MPG: 22.076923
 """
+
+trips = int(input("How many trips have you done?\n"))
+_trips = []
+for i in range(trips):
+    miles = float(input("Please enter miles driven for trip {}: ".format(i+1)))
+    gallons = float(input("Please enter gallons used for trip {}: ".format(i+1)))
+    mpg = miles/gallons
+    print("Total MPG for trip {} is: ".format(i+1), mpg)
+    _trips.append(mpg)
+    
+print("Total mog: {}".format(sum(_trips)))
