b84b4aeebc5a9c64ed19bdf09e8511d2dbd7d8f5
rx290 <asadwaseem32@gmail.com>
2022-05-05 15:27:33 +0500

    added a simple solution to CS Pset

--- Control Structures/Problem_12.py
+++ Control Structures/Problem_12.py
@@ -16,3 +16,20 @@ Grade is 5 if conditions are not met.
 
 write a program for grading of steel
 """
+
+hardness = int(input("Please Enter the Hardness of steel: "))
+carbon_content = float(input("Please Enter the Carbon content of steel: "))
+tensile_strength = int(input("Please Enter the Tensile strength of steel: "))
+
+if hardness == 10 and carbon_content < 0.7 and tensile_strength > 5600:
+    print("Steel is of grade 10!")
+elif (hardness == 10 and carbon_content <0.7):
+    print("Steel is of grade 9!")
+elif (carbon_content <0.7 and tensile_strength > 5600):
+    print("Steel is of grade 8!")
+elif (hardness == 10 and tensile_strength > 5600):
+    print("Steel is of grade 7!")
+elif (hardness == 10 or carbon_content < 0.7 or tensile_strength > 5600):
+    print("Steel is of grade 6!")
+else:
+    print("Steel is of grade 5")
