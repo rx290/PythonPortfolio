3d1a49f22c187533fa4c12b961e898c8e2fb303d
rx290 <asadwaseem32@gmail.com>
2022-05-12 02:09:21 +0500

    added new solutions to Control Structure Pset

--- Basics/Problem_9.py
+++ Basics/Problem_9.py
@@ -3,3 +3,9 @@ write a program to find the cost price of one item"""
 
 
 
+total_price = input("Please enter the total price of 15 items sold: ")
+profit = input("Please enter the total profit gained on 15 items: ")
+
+actual_cost = float(total_price ) - float(profit)
+
+print("Price of one item is: ",actual_cost/15)
--- Control Structures/Problem_3.py
+++ Control Structures/Problem_3.py
@@ -1,3 +1,17 @@
 """Write a program to determine whether the seller has made profit or incurred loss.
 Also determine how much profit he made or loss he incurred.
 AlsoCost price and selling price of an item is input by the user"""
+
+item_cost = float(input("Please enter item cost: "))
+selling_cost = float(input("Please enter selling cost: "))
+
+difference = selling_cost - item_cost
+
+if difference > 0:
+    print("Item was sold in profit!")
+    print("Profit was: ",difference)
+elif difference == 0:
+    print("Item was sold on terms of no profit and no gain!")
+else:
+    print("Item was sild in loss")
+    print("loss was: ",difference)
