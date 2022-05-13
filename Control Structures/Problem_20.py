95c3508933c74bedc09cd56cbf5c11d27302ad93
Asad <asadwaseem32@gmail.com>
2022-05-14 01:11:59 +0500

    added problem 20 solution of CS Pset

--- Control Structures/Problem_20.py
+++ Control Structures/Problem_20.py
@@ -1,3 +1,24 @@
 """
 Write a temperature conversion program but with a menu.
 """
+
+from os import system
+
+
+print("We offer following conversions:\n1. Fahrenheit to Celsius Conversion\n2. Celsius to Fahrenheit Conversion\n3, Quit\n")
+
+choice = int(input("Please enter your choice: "))
+
+if choice == 3:
+    exit()
+elif choice ==2:
+    celsius = float(input("Please enter temperature in Celsius: "))
+    fahrenheit = (9/5 * celsius) + 32
+    print("{} celsius is {} in fahrenheit.".format(celsius,fahrenheit))
+elif choice == 1:
+    fahrenheit = float(input("Please enter temperature in Fahrenheit: "))
+    celsius = 5/9 * (fahrenheit - 32) 
+    print("{} celsius is {} in fahrenheit.".format(celsius,fahrenheit))
+else:
+    print("Wrong choice entered!\nProgram is quitting!")
+    exit()
