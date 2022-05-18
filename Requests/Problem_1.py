3c68f7250acb88d21b117f1d85bf7aefb4a31c01
Asad <asadwaseem32@gmail.com>
2022-05-19 00:44:54 +0500

    added 2 solutions to Requests

--- Requests/Problem_1.py
+++ Requests/Problem_1.py
@@ -1,1 +1,12 @@
 """Write a Python code to find the Requests module - version, licence, copyright information, author, author email, document url, title and description""" 
+import requests 
+
+print("Module version:",requests.__version__)
+print("License:",requests. __license__)
+print("Copyright:",requests.__copyright__)
+print("Author:",requests.__author__)
+print("Author email:",requests.__author_email__)
+print("Document url, title, description:")
+print(requests.__url__)
+print(requests.__title__)
+print(requests.__description__)
--- Requests/Problem_2.py
+++ Requests/Problem_2.py
@@ -1,1 +1,38 @@
-"""Write a Python code to check the status code issued by a server in response to a client's request made to the server. Print all of the methods and attributes available to objects on successful request.""" 
+"""Write a Python code to check the status code issued by a server in response to a client's request made to the server. 
+Print all of the methods and attributes available to objects on successful request.""" 
+import  requests
+getter  = requests.get('https://api.github.com/events')
+print("Status Code: ",getter.status_code)
+print("All Methods: ",dir(getter))
+
+
+"""
+Types of requests
+    getter  = requests.get('https://api.github.com/events')
+    Creator = requests.post("https://httpbin.org/post", data=payload)
+    Editor = requests.put('https://httpbin.org/put', data={'key': 'value'})
+    Eliminator = requests.delete('https://httpbin.org/delete')
+    Essentials = requests.head('https://httpbin.org/get')
+    Options = requests.options('https://httpbin.org/get')
+
+Code:
+    1xx informational response: the request was received, continuing process
+    2xx successful: the request was successfully received, understood, and accepted
+    3xx redirection: further action needs to be taken in order to complete the request
+    4xx client error: the request contains bad syntax or cannot be fulfilled
+    5xx server error: the server failed to fulfil an apparently valid request
+    
+Most Used Examples:
+
+    200:    Read or okay
+    201:    Created or posted
+    301:    Moved Permanently
+    400:    Bad Request
+    401:    Unauthorized
+    404:    Not Found
+    500:    Internal Server error
+    501:    Not Implemented
+    502:    Bad Gateway    
+    503:    Service Unavailable
+
+"""
