b9f7ab372dcced43d74622bac510578523bc9b7a
Asad <asadwaseem32@gmail.com>
2022-05-29 00:48:41 +0500

    added some solutions

--- Data Structures/Tupples/Problem_12.py
+++ Data Structures/Tupples/Problem_12.py
@@ -1,1 +1,10 @@
 """ Write a Python program to count the elements in a list until an element is a tuple. """ 
+lst = [1,2,3,(2,),4,5,[1],6,'a','v',(2,)]
+counter =0
+for i in lst:
+    if type(i) == tuple:
+        break
+    else:
+        counter = counter +1
+        
+print(counter)
