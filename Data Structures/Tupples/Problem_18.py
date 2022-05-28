0ea4153e2075fdd6a5eeeaba02f03263a3a12570
Asad <asadwaseem32@gmail.com>
2022-05-28 14:15:38 +0500

    added some solution sets to tupple pset

--- Data Structures/Tupples/Problem_18.py
+++ Data Structures/Tupples/Problem_18.py
@@ -7,3 +7,20 @@ Check if White present in said tuple of tuples!
 True
 Check if Olive present in said tuple of tuples!
 False """ 
+tup = (('Red', 'White', 'Blue'), ('Green', 'Pink', 'Purple'), ('Orange', 'Yellow', 'Lime'))
+# for checking single element
+validator = lambda x,y: print(True) if x in y else print(False)
+col = ['White','Blue','Green']
+tup_ = tuple(map(validator,col,tup))
+print(tup_)
+pup = None
+
+# # for all item list
+# for i in range(len(col)):
+#     for j in range(len(tup)):
+#         if col[i] in tup[j]:
+#             print(True)
+#         else:
+#             print(False)
+
+# set something which will search for all items 
--- Data Structures/Tupples/Problem_8.py
+++ Data Structures/Tupples/Problem_8.py
@@ -3,3 +3,4 @@ Write a Python program to print a tuple with string formatting.
 Sample tuple : (100, 200, 300)
 Output : This is a tuple (100, 200, 300)
 """ 
+print("This is a tuple",(100, 200, 300))
