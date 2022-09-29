9a28415f400f94294ce31d4175f9aca8e9c4798a
rx290 <asadwaseem32@gmail.com>
2022-09-30 00:38:29 +0500

    added a simple solution in list/array folder

--- Data Structures/Arrays/Problem_5.py
+++ Data Structures/Arrays/Problem_5.py
@@ -1,7 +1,10 @@
 """
-Find the minimum and maximum out of the array!
+Find the minimum and maximum out of the array/list!
 a)  use built-in functions
 b)  Don't use built-in functions
 """
-import  array
 
+lst = [0.425,1,2,85,45854,0.1112,-85452,-0.5658,1000,23,-90000,666]
+print("Max: {}, Min: {}".format(max(lst),min(lst)))
+
+# B to be implemented
