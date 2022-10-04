215b45e1d48c7b0ceb812de5c13c01c79d196114
rx290 <asadwaseem32@gmail.com>
2022-10-05 00:13:14 +0500

    added bubble sort solution

--- Algorithms/Sorting/Bubble Sort/Problem_1.py
+++ Algorithms/Sorting/Bubble Sort/Problem_1.py
@@ -9,3 +9,17 @@ It can be practical if the input is usually in sort order but may occasionally h
 Sample Data: [14,46,43,27,57,41,45,21,70]
 Expected Result: [14, 21, 27, 41, 43, 45, 46, 57, 70]
 """
+
+
+sample_data = [14,46,43,27,57,41,45,21,70]
+expected_result =[]
+ele = sample_data[0]
+for i in range(len(sample_data)-1,0,-1):
+    for j in range(i):
+        if sample_data[j] >= sample_data[j+1]:
+            sample_data[j],sample_data[j+1] = sample_data[j+1],sample_data[j]
+        else:
+            pass
+
+        
+print(sample_data)
--- Algorithms/Sorting/Bubble Sort/Problem_2.py
+++ Algorithms/Sorting/Bubble Sort/Problem_2.py
@@ -1,1 +1,20 @@
 "Write a Python program to sort unsorted numbers using Recursive Bubble Sort." 
+sample =[14, 21, 27, 41, 43, 45, 46, 57, 70]
+
+def sorter(sample:list):
+    choice = input("Please enter how you want your list to be sorted:\n1. Ascending Order\n2. Descending Order\nYour Choice: ")
+    if choice == '1':
+        for i in range(len(sample)-1,0,-1):
+            for j in range(i):
+                if sample[j] >= sample[j+1]:
+                    sample[j],sample[j+1] = sample[j+1],sample[j]
+    elif choice == '2':
+        for i in range(len(sample)-1,0,-1):
+            for j in range(i):
+                if sample[j] <= sample[j+1]:
+                    sample[j],sample[j+1] = sample[j+1],sample[j]
+    else:
+        print("Invalid Choice!")
+    return print(sample)
+
+sorter(sample)
