be3b500185ee0ce230a01621fe8bbc42e9c9a2c8
rx290 <asadwaseem32@gmail.com>
2023-09-28 02:03:13 +0500

    added problem 3 for date time

--- Date Time/Problem_3.py
+++ Date Time/Problem_3.py
@@ -1,3 +1,8 @@
 """ Write a Python program to convert a string to datetime. 
-Sample String : Jan 1 2014 2:43PM
-Expected Output : 2014-07-01 14:43:00 """ 
+Sample String : Jan 1 2014 2:43 PM
+Expected Output : 2014-01-01 14:43:00 """ 
+
+from datetime import datetime
+date_time = input("Please enter your date time string: ")
+converted_time = datetime.strptime(date_time, '%b %d %Y %I:%M %p')
+print(converted_time.strftime("%Y-%m-%d %H:%M:%S "))
