8fb6c9a66649212d65bd6b3ba2cebdd99c40e95c
rx290 <asadwaseem32@gmail.com>
2023-09-30 22:20:54 +0500

    added one module

--- Date Time/Problem_13.py
+++ Date Time/Problem_13.py
@@ -1,3 +1,9 @@
 """Write a Python program to get week number. 
 Sample Date : 2015, 6, 16
 Expected Output : 25 """ 
+
+from datetime import datetime
+
+currentdate = input("Please enter current date: ")
+datetimeObj= datetime.strptime(currentdate, '%Y,%m,%d')
+print(datetime.strftime(datetimeObj,'%U'))
