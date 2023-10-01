9437321e4ca0eb14d5e86476cb018f3fbdee6240
rx290 <asadwaseem32@gmail.com>
2023-10-02 02:11:55 +0500

    added task15 b

--- File Handling/file_handling_basic/Problem_15.py
+++ File Handling/file_handling_basic/Problem_15.py
@@ -3,17 +3,24 @@ a. Write a Python program to generate 26 text files named A.txt, B.txt, and so o
 
 b. Write a Python program to create a file where all letters of English alphabet are listed by specified number of letters on each line.
 """ 
-import os
+# import os
+# from string import ascii_letters
+# directory = 'AlphabetFiles'
+# parentDirectory = "./File Handling/file_handling_basic/"
+# path=os.path.join(parentDirectory,directory)
+# os.mkdir(path)
+# path = path+'/'
+# global filename
+# filename = ''
+# for i in range(26,52):
+#     letter = ascii_letters[i]
+#     filename = path+letter+'.txt'
+#     with open(filename,'w') as e:
+#         pass
+
 from string import ascii_letters
-directory = 'AlphabetFiles'
-parentDirectory = "./File Handling/file_handling_basic/"
-path=os.path.join(parentDirectory,directory)
-os.mkdir(path)
-path = path+'/'
-global filename
-filename = ''
-for i in range(26,52):
-    letter = ascii_letters[i]
-    filename = path+letter+'.txt'
-    with open(filename,'w') as e:
-        pass
+with open("./File Handling/file_handling_basic/task15b.txt",'a') as f:
+    for i in range(26,52):
+        alphabetString = "Specified Number for Letter "+ascii_letters[i]+ " is "+str(i)+'\n'
+        f.write(alphabetString)
+    
