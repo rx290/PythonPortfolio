c0b0af9335802e09c219238bf7930391c2542245
rx290 <asadwaseem32@gmail.com>
2023-10-01 14:58:49 +0500

    added task 8 for File Handling Basic

--- File Handling/file_handling_basic/Problem_8.py
+++ File Handling/file_handling_basic/Problem_8.py
@@ -1,1 +1,8 @@
 "Write a Python program to copy the contents of a file to another file " 
+
+
+with open("./File Handling/file_handling_basic/sample.txt","r") as f:
+    with open("./File Handling/file_handling_basic/DestinationFile.txt","a+") as e:
+        lines = [lines for lines in f]
+        for line in lines:
+            e.write(line)
