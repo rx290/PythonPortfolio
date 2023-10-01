96a6683258a59bfbee8a044b2f09a68ca5670558
rx290 <asadwaseem32@gmail.com>
2023-10-02 01:59:26 +0500

    added task 15 a

--- File Handling/file_handling_basic/Problem_15.py
+++ File Handling/file_handling_basic/Problem_15.py
@@ -3,3 +3,17 @@ a. Write a Python program to generate 26 text files named A.txt, B.txt, and so o
 
 b. Write a Python program to create a file where all letters of English alphabet are listed by specified number of letters on each line.
 """ 
+import os
+from string import ascii_letters
+directory = 'AlphabetFiles'
+parentDirectory = "./File Handling/file_handling_basic/"
+path=os.path.join(parentDirectory,directory)
+os.mkdir(path)
+path = path+'/'
+global filename
+filename = ''
+for i in range(26,52):
+    letter = ascii_letters[i]
+    filename = path+letter+'.txt'
+    with open(filename,'w') as e:
+        pass
