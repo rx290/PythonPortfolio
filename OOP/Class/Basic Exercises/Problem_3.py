d02e9483a4d0c0e807ce7199d9fd2d5c6bee33e4
rx290 <asadwaseem32@gmail.com>
2023-10-04 00:26:07 +0500

    added two new tasks

--- OOP/Class/Basic Exercises/Problem_3.py
+++ OOP/Class/Basic Exercises/Problem_3.py
@@ -1,1 +1,10 @@
 "Write a Python program to create an instance of a specified class and display the namespace of the said instance." 
+
+class MySampleClass:
+    def SampleMethod(*args):
+        pass
+    def HelloWorld(*args):
+        return print("Hello World!")
+    
+instance = MySampleClass()
+print(dir(instance))
--- OOP/Class/Basic Exercises/Problem_4.py
+++ OOP/Class/Basic Exercises/Problem_4.py
@@ -3,3 +3,28 @@
 Write a python program which import the abs() function using the builtins module, 
 display the documentation of abs() function and find the absolute value of -155. 
 """
+
+# Builtin Documentation
+helpDoc_Abs = abs.__doc__
+print(helpDoc_Abs)
+print(abs(-155))
+
+# As help document are not that helpful lets use IO and contextlib module to actually print out the help doc for any function needed
+
+import contextlib
+import io
+
+class HelpDocPrinter:
+  def PrintHelp(self,MethodName):
+      # String IO is an in memory file-like object, it is called Memory files or String Buffers
+      helpDoc = io.StringIO()
+      # Redirecting sys.stout or system standard output to any Memory Files
+      with contextlib.redirect_stdout(helpDoc):
+          help(MethodName)
+      helpDoc.seek(0)
+      helpDocVar = helpDoc.read()
+      return helpDocVar
+  
+docHelper = HelpDocPrinter()
+absHelpDoc=docHelper.PrintHelp(abs)
+print(absHelpDoc)
