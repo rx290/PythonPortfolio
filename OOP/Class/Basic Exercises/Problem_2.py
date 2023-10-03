6a47a1022094617a68cf599b629f3a09741ff795
rx290 <asadwaseem32@gmail.com>
2023-10-03 22:51:09 +0500

    added task 2

--- OOP/Class/Basic Exercises/Problem_2.py
+++ OOP/Class/Basic Exercises/Problem_2.py
@@ -1,1 +1,11 @@
 "Write a Python program to create a class and display the namespace of the said class." 
+
+class MySampleClass:
+    def Method1(args):
+        pass
+    def Method2(args):
+     pass
+    def Method3(args):
+     pass
+
+print(dir(MySampleClass))
