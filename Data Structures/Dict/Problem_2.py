cacc16941bc2220b68423b7b59ac3cf7ea7a80c3
rx290 <asadwaseem32@gmail.com>
2023-10-05 10:21:11 +0500

    added 1 task for dict

--- Data Structures/Dict/Problem_2.py
+++ Data Structures/Dict/Problem_2.py
@@ -7,3 +7,20 @@ Write a function to print the data of all 200 accounts
 Write a function to print data of any given account number
 Write a menu to deposit or withdraw money
 """
+customerData = {
+    'customer1':{
+        'Account': 000,
+        'Name': '',
+        'Amount':0.0,
+    }
+}
+
+def ReturnAllData():
+    return customerData
+
+def AccountFinder(Key,accountNumber):
+    index = customerData.get(Key,accountNumber)
+    return index
+
+print(ReturnAllData())
+print(AccountFinder('Account',000))
