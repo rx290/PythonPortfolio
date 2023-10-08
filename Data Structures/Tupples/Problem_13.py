5eae2794af1de829e29ce40b447da08c4ac156dc
rx290 <asadwaseem32@gmail.com>
2023-10-08 21:09:03 +0500

    added task 13 for tuple

--- Data Structures/Tupples/Problem_13.py
+++ Data Structures/Tupples/Problem_13.py
@@ -4,3 +4,7 @@ Original string: python 3.0
 Convert the said string to a tuple:
 ('p', 'y', 't', 'h', 'o', 'n', '3', '.', '0')
 <class 'tuple'> """ 
+input_string = "Python 3.0"
+convertedTup = [*input_string.replace(' ','')]
+convertedTup = tuple(convertedTup)
+print(convertedTup)
