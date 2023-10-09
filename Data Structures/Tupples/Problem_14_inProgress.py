ed11031eeafed963730f7da973f125f064b97653
rx290 <asadwaseem32@gmail.com>
2023-10-09 22:44:52 +0500

    added few tasks

--- Data Structures/Tupples/Problem_14_inProgress.py
+++ Data Structures/Tupples/Problem_14_inProgress.py
@@ -5,3 +5,12 @@ Product - multiplying all the numbers of the said tuple: -864
 Original Tuple:
 (2, 4, 8, 8, 3, 2, 9)
 Product - multiplying all the numbers of the said tuple: 27648""" 
+tup = (4, 3, 2, 2, -1, 18)
+def TupleProduct(tup,i,length):
+    product= 0
+    if i == length-1:
+        return product
+    else:
+        i += 1
+        TupleProduct(tup,i,length)
+    
--- Data Structures/Tupples/Problem_16.py
+++ Data Structures/Tupples/Problem_16.py
@@ -3,3 +3,13 @@ Original tuple values:
 (('333', '33'), ('1416', '55'))
 New tuple values:
 ((333, 33), (1416, 55)) """ 
+
+ogTuple = (('333', '33'), ('1416', '55'))
+ogTuple = list([list(i) for i in ogTuple])
+for i in range(0,len(ogTuple)):
+    for j in range(0,len(ogTuple[i])):
+        # i[j].replace(i[j], int(i[j]))
+        ogTuple[i][j] = int(ogTuple[i][j]) 
+    ogTuple[i]= tuple(ogTuple[i])
+ogTuple = tuple(ogTuple)
+print(ogTuple)
