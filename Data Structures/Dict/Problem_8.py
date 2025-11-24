5a96084cbe21ef199a21143c7d0d44431c36137c
Rx290 <asadwaseem32@gmail.com>
2025-11-24 20:19:35 +0500

    added some solutions

--- Data Structures/Dict/Problem_8.py
+++ Data Structures/Dict/Problem_8.py
@@ -5,3 +5,21 @@ dic1={1:10, 2:20}
 dic2={3:30, 4:40}
 dic3={5:50,6:60}
 Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}""" 
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+
+dic4={7:10, 8:20}
+dic5={9:30, 10:40}
+dic6={13:50,15:60}
+
+dic7={17:10, 20:20}
+dic8={22:30, 24:40}
+dic9={27:50,30:60}
+
+concatenated_dic = dic1 | dic2 |dic3
+print(concatenated_dic)
+
+concatenated_dic2 = {**dic4,**dic5,**dic6}
+print(concatenated_dic2)
--- Data Structures/Dict/Problem_9.py
+++ Data Structures/Dict/Problem_9.py
@@ -1,1 +1,7 @@
 """ Write a Python script to check whether a given key already exists in a dictionary.""" 
+
+dict_1 = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+_key = int(input("please enter the key you think is in the dictionary"))
+
+print("The key is in the Dictionary") if _key in dict_1 else print("The key is not in the Dictionary")
