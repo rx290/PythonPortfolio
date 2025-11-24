4b5658fe32e24e20b6219d4f50887a8b814db2f5
Rx290 <asadwaseem32@gmail.com>
2025-11-24 17:42:32 +0500

    added partial solution for problem 3 and full solution for problem 4 in Dict Dir

--- Data Structures/Dict/Problem_3.py
+++ Data Structures/Dict/Problem_3.py
@@ -4,3 +4,20 @@ Write a program to create an array of the structure and enter some data into it.
 Then ask the user to enter current date, display the names of those employees whose tenure is 3 r more than 3 years according 
 to the current date
 """
+
+import random
+
+# Re to use for parsing
+# ((0?[0-9]|[0-2])-([0-2][0-9]|3[0-1])-([1900-2025]{4}))
+
+
+def employee(name,date_of_joining,sex):
+    
+    return{
+        "employee_name": name,
+        "employee_code": random(),
+        "date_of_joining": date_of_joining,
+        "gender": sex,   
+    }
+    
+    
--- Data Structures/Dict/Problem_4.py
+++ Data Structures/Dict/Problem_4.py
@@ -2,3 +2,20 @@
 Write aa program that compares two given dates. To store data use structures say date that contains three members namely,
 date, month and years. If the dates are equal then display messages as "Equal" otherwise "Unequal"
 """
+
+def dates(day, month,year):
+    return{
+        "Day": day,
+        "Month": month,
+        "Year": year
+    }
+names= ['day','month','year']
+date_1= list(input("Please input date in following pattern (days Month Year):").split(sep=' '))
+date_2= list(input("Please input date in following pattern (days Month Year):").split(sep=' '))
+date_1 = dates(date_1[0],date_1[1],date_1[2])
+date_2 = dates(date_2[0],date_2[1],date_2[2])
+
+if (date_1==date_2):
+    print("Dates are equal!")
+else:
+    print("Dates are not equal")
