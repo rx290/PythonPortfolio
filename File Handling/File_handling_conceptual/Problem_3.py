27f69576f6abb2b014a6e905b3cdaea28d125881
Rx290 <asadwaseem32@gmail.com>
2025-11-25 09:19:03 +0500

    added few solutions

--- File Handling/File_handling_conceptual/Problem_3.py
+++ File Handling/File_handling_conceptual/Problem_3.py
@@ -1,3 +1,7 @@
 """
 Write a function in Python to count and display the total number of words in a text file
 """ 
+with open(file='/run/media/drago/Fast Drive/PythonPortfolio/File Handling/File_handling_conceptual/sample.txt', mode='r') as file:
+    corpus= file.read()
+    words = corpus.split()
+    print("Total number of words in the corpus are: ",len(words))
--- File Handling/File_handling_conceptual/Problem_4.py
+++ File Handling/File_handling_conceptual/Problem_4.py
@@ -6,3 +6,16 @@ For example: If the content of the file is:
 
 The output should be 5.
 """ 
+
+import re
+corpus = "India is the fastest-growing economy. India is looking for more investments around the globe. The whole world is looking at India as a great market. Most of the Indians can foresee the heights that India is capable of reaching."
+with open(file='/run/media/drago/Fast Drive/PythonPortfolio/File Handling/File_handling_conceptual/sample.txt',mode='a+') as file:
+    file.write(corpus)
+    file.close
+    
+with open(file='/run/media/drago/Fast Drive/PythonPortfolio/File Handling/File_handling_conceptual/sample.txt',mode='r') as of:
+    read_corpus = of.read()
+    checker = re.compile(r'\b[tT]he\b')
+    _str_checker = checker.findall(read_corpus)
+    print("Number of the in the corpus is: ",_str_checker)
+    of.close()
