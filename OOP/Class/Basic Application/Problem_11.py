f5c679f487aa8e7f19d34490283c3546cf09fdd8
Rx290 <asadwaseem32@gmail.com>
2025-11-27 17:32:33 +0500

    revised some classes tasks

--- OOP/Class/Basic Application/Problem_11.py
+++ OOP/Class/Basic Application/Problem_11.py
@@ -1,3 +1,24 @@
 """
 Write a Python class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle.
 """
+
+from math import pi
+
+class Circle:
+    def __init__(self, radius):
+        self.pi = pi
+        self.radius = radius
+        
+    def diameter(self):
+        return 2 * self.radius
+    
+    def area(self):
+        return pi * (self.radius**2)
+    
+    def perimeter(self):
+        return 2 * pi * self.radius
+    
+c = Circle(5)
+print("The diameter of the circle is {}, the area of the circler is {}, the circumference of a circle is {}.".format(c.diameter(),c.area(),c.perimeter()))
+
+
