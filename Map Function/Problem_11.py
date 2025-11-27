8ccc30e32b4296bd13e01f9abbd866e2b1796e62
rx290 <asadwaseem32@gmail.com>
2025-11-27 21:32:17 +0500

    added some exercises

--- Map Function/Problem_11.py
+++ Map Function/Problem_11.py
@@ -1,4 +1,10 @@
 """Write a Python program to compute the sum of elements of a given array of integers, use map() function.""" 
 
-_sum = 0
 lst = [1,2,3,4,5,6,7,8,9,10]
+
+def summer(x):
+    mapped_sum = map(lambda a:a,x)
+    _sum = sum(mapped_sum)
+    return _sum
+total_sum = summer(lst)
+print(total_sum)
