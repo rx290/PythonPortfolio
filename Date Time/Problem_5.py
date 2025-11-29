94c4708176aa65edf8d76a238cb88815be83e941
rx290 <asadwaseem32@gmail.com>
2025-11-29 20:58:00 +0500

    added few solutions

--- Date Time/Problem_5.py
+++ Date Time/Problem_5.py
@@ -2,3 +2,10 @@
 Sample Date :
 Current Date : 2015-06-22
 5 days before Current Date : 2015-06-17 """ 
+
+from datetime import timedelta, date
+
+current_date = date.today()
+date_after_5_days = current_date + timedelta(days=-5)
+
+print("Current Date:",current_date,"\n5 days before Current Date: ",date_after_5_days)
